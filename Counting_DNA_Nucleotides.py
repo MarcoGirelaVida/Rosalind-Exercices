2917df3b24f3286a6c0d27e67bdfe705b5fd3872
Marco_Girela <marcogirelavida@gmail.com>
2023-05-20 14:04:25 +0200

    .

---
+++ Counting_DNA_Nucleotides.py
@@ -0,0 +1,14 @@
+'''
+EXERCICE 1 - ROSALIND - Counting DNA Nucleotides
+DATE: 19-05-2023
+AUTOR: Marco Girela Vida
+
+PROBLEM:
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+Given: A DNA string s of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+'''
